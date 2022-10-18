Kyligence/spark

AL-6084 in Cast for method of canCast, when DecimalType cast to DoubleType add transformable logic (#542)
* AL-6084 in Cast for method of canCast, when DecimalType cast DecimalType to DoubleType add suit logical

sql/catalyst/src/main/scala/org/apache/spark/sql/types/DecimalType.scala
@@ -93,6 +93,8 @@ case class DecimalType(precision: Int, scale: Int) extends FractionalType {
       (precision - scale) <= (dt.precision - dt.scale) && scale <= dt.scale
     case dt: IntegralType =>
       isTighterThan(DecimalType.forType(dt))
+    case dt: DoubleType =>
+      isTighterThan(DecimalType(precision, scale))
     case _ => false
   }
 

jinyishu/kylin-spark

sql/catalyst/src/main/scala/org/apache/spark/sql/types/DecimalType.scala
@@ -100,6 +100,8 @@ case class DecimalType(precision: Int, scale: Int) extends FractionalType {
       (precision - scale) <= (dt.precision - dt.scale) && scale <= dt.scale
     case dt: IntegralType =>
       isTighterThanInternal(DecimalType.forType(dt))
+    case dt: DoubleType =>
+      isTighterThanInternal(DecimalType(precision, scale))
     case _ => false
   }
 